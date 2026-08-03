33092211a90ce4aa942ab62fb23339582387a5cf
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2026-08-03 07:03:40 +0530

    terraform: update 1 file(s) (main.tf)

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -3,7 +3,9 @@ resource "azurerm_managed_redis" "example" {
   resource_group_name = var.rg_name
   location            = var.rg_location
   sku_name            = var.sku_name
-  default_database    = {}
+  default_database = {
+    client_protocol = "Plaintext"
+  }
 }
 
 
